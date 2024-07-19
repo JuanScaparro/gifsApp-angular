07599932ff8275e8d811fd2394ab6dfa2cf08faf
Juan M Scaparro <juanscaparro@gmail.com>
2024-07-19 17:11:42 +0200

    create Gifs App exercice

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,62 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GifsService {
+  public gifsList: Gif[] = [];
+
+  private _tagHistory: string[] = [];
+  private apiKey: string = 'Er1RCrtOde64DRGm3iqvPdDEe9GMet5x';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
+
+  constructor(private http: HttpClient) {
+    this.loadLS();
+  }
+
+  get tagHistory(): string[] {
+    return [...this._tagHistory];
+  }
+
+  private organizeHistory(tag: string): void {
+    tag = tag.toLowerCase();
+    if (this._tagHistory.includes(tag)) {
+      this._tagHistory = this._tagHistory.filter((oldTag) => oldTag !== tag);
+    }
+    this._tagHistory.unshift(tag);
+    this._tagHistory = this._tagHistory.splice(0, 10);
+    this.saveLS();
+  }
+
+  private saveLS(): void {
+    localStorage.setItem('history', JSON.stringify(this._tagHistory));
+  }
+
+  private loadLS(): void {
+    if (!localStorage.getItem('history')) return;
+
+    this._tagHistory = JSON.parse(localStorage.getItem('history')!);
+
+    if (this._tagHistory.length === 0) return;
+    this.searchTag(this._tagHistory[0]);
+  }
+
+  searchTag(tag: string): void {
+    if (tag.length === 0) return;
+    this.organizeHistory(tag);
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '12')
+      .set('q', tag);
+
+    this.http
+      .get<SearchResponse>(`${this.serviceUrl}/search?`, { params })
+      .subscribe((resp) => {
+        this.gifsList = resp.data;
+      });
+  }
+}
